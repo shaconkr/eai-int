893877ee8bcddde018e523600de8d79b0d383fd2
이석규 <cis089@hyundaidept.com>
2023-02-17 15:36:20 +0900

    안정화

---
+++ TADCIS201.module/src/com/hcis/eai/tad/TADCISHelper.java
@@ -0,0 +1,60 @@
+package com.hcis.eai.tad;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hcis.eai.common.HCISHelper;
+
+import kr.shacon.format.TibXsdParser;
+
+
+public class TADCISHelper<N> extends HCISHelper<N> {
+	
+    private static final Logger log = LoggerFactory.getLogger(TADCISHelper.class);	
+	
+	@Override
+    public String toEDI(String msgType, String jsonString, String encoding, String xml) throws Exception {
+        String encodedString = new String(jsonString.getBytes(encoding), encoding);
+        String interfaceId = getInterfaceId();
+        TADCISTransformer trans = new TADCISTransformer(interfaceId);
+        String streamName = (msgType.startsWith("_")) ? interfaceId + msgType : msgType;
+        log.debug("## streamName [{}] xml [{}] msgType [{}] ", streamName, xml, msgType);
+        String edi =  trans.toEDI(streamName, encodedString, encoding, xml);        
+        return new String(setTotalLength(edi.getBytes(encoding), 8, 0, false, encoding),encoding); 
+    }
+	
+	@Override	
+    public String toJSON(String msgType, String ediString, String encoding, String xml) throws Exception {
+        String interfaceId = getInterfaceId();
+        TADCISTransformer trans = new TADCISTransformer(interfaceId);
+        String streamName = (msgType.startsWith("_")) ? interfaceId + msgType : msgType;
+        return new String(trans.toJSON(streamName, ediString, encoding, null).getBytes(encoding), encoding);
+    }
+	
+	@Override
+	public String logEDIBytes(byte[] bytes, String xsdpath) {
+		InputStream is = getClass().getResourceAsStream(xsdpath);
+		try {
+			if(is.available() > 0 ) {
+				log.debug("@@@ Found O {}", xsdpath);
+			}else {
+				log.debug("@@@ Not Found O {}", xsdpath);
+			}
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		TibXsdParser parser = new TibXsdParser();
+    	List<Map<String, Object>> layout = parser.parseXsdFromClasspath(is, "root");
+    	StringBuffer sb = new StringBuffer();
+    	int offset = 0;
+    	logEDIItem(sb, layout, bytes, offset);    	
+    	return sb.toString();
+    }
+}
