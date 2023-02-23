66cf3712ffe3405f22590fe4bf6d5133d78a89b8
이석규 <cis089@hyundaidept.com>
2023-02-23 17:33:31 +0900

    I/F 수정/테스트 CISTADAHFA0100101 특정회원내역조회(승인서버) CISTADAHHE0080101 점별 승인자별 승인결과 집계(승인서버)

--- CISTAD201.module/src/com/hcis/eai/tad/CISTADHelper.java
+++ CISTAD201.module/src/com/hcis/eai/tad/CISTADHelper.java
@@ -1,40 +1,132 @@
 package com.hcis.eai.tad;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.hcis.eai.common.HCISHelper;
+import com.hcis.eai.common.types.GsonMapDeserializer;
 
 import kr.shacon.format.TibXsdParser;
+import kr.shacon.util.CastUtils;
 
 
 public class CISTADHelper<N> extends HCISHelper<N> {
 	
-    private static final Logger log = LoggerFactory.getLogger(CISTADHelper.class);	
-	
+    private static final Logger log = LoggerFactory.getLogger(CISTADHelper.class);
+    private final Gson  gson = new GsonBuilder()
+            .setPrettyPrinting()
+            .registerTypeAdapter(Map.class, new GsonMapDeserializer())
+            .setDateFormat("yyyy-MM-dd HH:mm:ss")
+            .serializeNulls()
+            .create();
+
+    /**
+     * target 전문으로 변경 ( target header )
+     * 
+     * Inbound to Outbound Message Mapping
+     *
+     * Json Layout ( header - data )
+     * header mapping ( target 기준 존재 하는 항목만 , 진행번호증가, 헤더길이)
+     * data bypass
+     *
+     * @return
+     */
+	@Override
+    @SuppressWarnings("unchecked")
+    public String baseMapping(String targetSystemId, String jsonString) {
+        Map<String,Object> sourceMessage= gson.fromJson(jsonString, Map.class);
+        Map<String,Object> sourceHeader = CastUtils.cast((Map<?, ?>) sourceMessage.get("header"));
+        Map<String,Object> sourceBody   = CastUtils.cast((Map<?, ?>) sourceMessage.get("data"));
+        Map<String,Object> targetHeader = new LinkedHashMap<>();
+
+        List<Map<String, Object>> targetHeaderLayout =  loadSystemHeader(targetSystemId);
+        int headerLength = 0;
+        for (Map<String, Object> m : targetHeaderLayout) {
+            int colLen = Integer.parseInt(String.valueOf(m.get("length")));
+            headerLength += colLen;
+            String key = String.valueOf(m.get("name"));
+            targetHeader.put(key, Objects.isNull(sourceHeader.get(key))?new String():sourceHeader.get(key));
+        }
+        targetHeader.put("prgrNo", Integer.parseInt(String.valueOf(targetHeader.get("prgrNo"))) + 1); // 진행번호증가
+        targetHeader.put("hdrLen", headerLength); // system 별 header 길이 게산
+
+        return gson.toJson(ImmutableMap.of("header", targetHeader, "data", sourceBody));
+    }
+
+//	@Override
+//	public List<Map<String, Object>> loadSystemHeader(String sysId) {
+//    	TibXsdParser parser = new TibXsdParser();        
+//        String headPath     = "Schemas/" +sysId + "Header.xsd";
+//        Class<?> ancClass   = HCISHelper.class;
+//        
+//        InputStream is = ancClass.getClass().getResourceAsStream(headPath);    	
+//        if( is == null) {
+//        	log.error("System Header Not Found  {}", headPath);
+//        }
+//        return parser.parseXsdFromClasspath(is, "header");
+//	}
+
+	@Override
+	public List<Map<String, Object>> loadSystemHeader(String sysId) {
+        String headPath   = "Schemas/" +sysId + "Header.xsd";
+        Class<?> ancClass = HCISHelper.class;
+        URL location   = ancClass.getProtectionDomain().getCodeSource().getLocation();
+        String ancPath = location.getPath();
+        String path    = ancPath + headPath;
+
+        InputStream is = null;
+        List<Map<String, Object>> rtnMap = null;
+        try {
+        	is = new FileInputStream(new File(path));
+        	TibXsdParser parser = new TibXsdParser();
+        	rtnMap = parser.parseXsdFromClasspath(is, "header");
+        	is.close();
+        } catch (FileNotFoundException e) {
+        	log.error("System Header Not Found  {}", path);
+        } catch (IOException e) {
+        	e.printStackTrace();
+        }
+
+        return rtnMap;
+	}
+
+	@Override
+    public String EDIbytesToJson(String msgType, byte[] ediBytes, String encoding, String xml) throws Exception {
+log.debug("sbYi  0 -----------------  CISTADHelper.EDIbytesToJson ");
+        return toJSON(msgType, new String(ediBytes, encoding), encoding, xml );
+    }
+
 	@Override
     public String toEDI(String msgType, String jsonString, String encoding, String xml) throws Exception {
         String encodedString = new String(jsonString.getBytes(encoding), encoding);
-        String interfaceId = getInterfaceId();
+        String interfaceId   = getInterfaceId();
         CISTADTransformer trans = new CISTADTransformer(interfaceId);
         String streamName = (msgType.startsWith("_")) ? interfaceId + msgType : msgType;
         log.debug("## streamName [{}] xml [{}] msgType [{}] ", streamName, xml, msgType);
-        String edi =  trans.toEDI(streamName, encodedString, encoding, xml);        
+        String edi =  trans.toEDI(streamName, encodedString, encoding, xml);
         return new String(setTotalLength(edi.getBytes(encoding), 8, 0, false, encoding),encoding); 
     }
 	
-	@Override	
+	@Override
     public String toJSON(String msgType, String ediString, String encoding, String xml) throws Exception {
-log.debug("sbYi --------  CISTADHelper.toJSON  --------------  ediString : {}", ediString);
-log.debug("sbYi --------  CISTADHelper.toJSON  -----  ediString.length() : {}", ediString.length());
         String interfaceId = getInterfaceId();
         CISTADTransformer trans = new CISTADTransformer(interfaceId);
         String streamName = (msgType.startsWith("_")) ? interfaceId + msgType : msgType;
+log.debug("sbYi  3 -----------------  CISTADHelper.toJSON        streamName : {} ", streamName);
         return new String(trans.toJSON(streamName, ediString, encoding, xml).getBytes(encoding), encoding);
 //        return new String(trans.toJSON(streamName, ediString, encoding, null).getBytes(encoding), encoding);
     }
@@ -44,9 +136,9 @@ log.debug("sbYi --------  CISTADHelper.toJSON  -----  ediString.length() : {}", 
 		InputStream is = getClass().getResourceAsStream(xsdpath);
 		try {
 			if(is.available() > 0 ) {
-				log.debug("@@@ Found O {}", xsdpath);
+				log.debug("@@@   CISTADHelper   Found O {}", xsdpath);
 			}else {
-				log.debug("@@@ Not Found O {}", xsdpath);
+				log.debug("@@@   CISTADHelper   Not Found O {}", xsdpath);
 			}
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
--- CISTAD201.module/src/com/hcis/eai/tad/CISTADTransformer.java
+++ CISTAD201.module/src/com/hcis/eai/tad/CISTADTransformer.java
@@ -2,17 +2,32 @@ package com.hcis.eai.tad;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 
+import org.beanio.Marshaller;
 import org.beanio.StreamFactory;
 import org.beanio.internal.util.IOUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import com.google.common.base.Strings;
 import kr.shacon.format.Transformer;
+import kr.shacon.types.MapDeserializer;
 
 public class CISTADTransformer extends Transformer {
     private static final Logger log = LoggerFactory.getLogger(Transformer.class);
-	
+    StreamFactory factory2;
+
+    protected Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .registerTypeAdapter(Map.class, new MapDeserializer())
+                .setDateFormat("yyyy-MM-dd HH:mm:ss")
+                .serializeNulls()
+                .create();
+
 	public CISTADTransformer(String ifId) {
 		super(ifId);
 		// TODO Auto-generated constructor stub
@@ -20,16 +35,17 @@ public class CISTADTransformer extends Transformer {
 
 	@Override
 	protected StreamFactory newStreamFactory(String xmlpath) {
-    	log.debug("@@@ load xml {}", xmlpath);
+    	log.debug("@@@ CISTADTransformer load xml {}", xmlpath);
         StreamFactory factory = StreamFactory.newInstance();
 		InputStream is = getClass().getResourceAsStream(xmlpath);
 		try {
-			if(is.available() > 0 ) {
-				log.debug("@@@ Found O {}", xmlpath);
-			}else {
-				log.debug("@@@ Not Found O {}", xmlpath);
+			if (is.available() > 0) {
+				log.debug("@@@   CISTADTransformer   Found O {}", xmlpath);
+			} else {
+				log.debug("@@@   CISTADTransformer   Not Found O {}", xmlpath);
 			}
 		} catch (IOException e1) {
+			log.debug("@@@   CISTADTransformer   IOException e1 {}", e1.toString());
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
@@ -47,9 +63,34 @@ public class CISTADTransformer extends Transformer {
 
 	@Override
 	public String toEDI(String msgType, String jsonString, String encoding, String xml) {
-		// TODO Auto-generated method stub
+log.debug("sbYi ------------------  CISTADTransformer.toEDI ");
+//		return toEDI2(msgType, jsonString, encoding, xml);
 		return super.toEDI(msgType, jsonString, encoding, xml);
 	}
+
+	public String toEDI2(String msgType, String jsonString, String encoding, String xml) {
+        if(!Strings.isNullOrEmpty(xml)) this.factory2 = newStreamFactory(xml);
+log.debug("sbYi 0 ----------------  CISTADTransformer.toEDI2    msgType : {}", msgType);
+        Marshaller marshaller = factory2.createMarshaller(msgType);
+log.debug("sbYi 1 ----------------  CISTADTransformer.toEDI2 jsonString : {}", jsonString);
+log.debug("sbYi 1 ----------------  CISTADTransformer.toEDI2   encoding : {}", encoding);
+        Map<String, Object> map = gson.fromJson(jsonString, Map.class);
+log.debug("sbYi 2 ----------------  CISTADTransformer.toEDI2        map : {}", map);
+
+Marshaller ms = null;
+try {
+ms = marshaller.marshal(map, encoding);
+} catch (Exception e) {
+System.out.println("s ------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+System.out.println(e);
+System.out.println("m ------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+System.out.println(e.toString());
+System.out.println("e ------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+}
+
+String rtn = ms.toString();
+        return rtn;
+    }
 
 	@Override
 	public String toJSON(String msgType, String ediString, String encoding, String xml) {
